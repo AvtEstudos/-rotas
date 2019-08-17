793de711e76bacff071a6cdaa96f2d3c30fe80b3
AVT Estudos <andretamayoshi@gmail.com>
2019-08-17 20:37:28 -0300

    53 - Rotas: parâmetros de roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
   { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent }  
+  { path: 'cursos', component: CursosComponent },
+  { path: 'cursos/:id', component: CursoDetalheComponent }  
 ];
 
 @NgModule({
