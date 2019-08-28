b6e953f9c2176c7113d3134cd903ae058646cb35
AVT Estudos <andretamayoshi@gmail.com>
2019-08-27 22:47:48 -0300

    57 - Rotas: Criando um módulo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
@@ -7,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { LoginComponent } from './login/login.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
