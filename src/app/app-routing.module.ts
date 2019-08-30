c396aa8cfbaf68919008b5f0b7f588694c2d9e26
AVT Estudos <andretamayoshi@gmail.com>
2019-08-30 20:51:43 -0300

    59-Criando um módulo de rotas de funcionalidade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent  },
-  { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },  
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent }
+  { path: 'login', component: LoginComponent }  
 ];
 
 @NgModule({
