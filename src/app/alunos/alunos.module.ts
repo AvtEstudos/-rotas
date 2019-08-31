a3d1fcca4281f274515f41acfeb3555f19cc92a3
AVT Estudos <andretamayoshi@gmail.com>
2019-08-31 17:27:54 -0300

    60-Rotas Filhas

---
+++ src/app/alunos/alunos.module.ts
@@ -0,0 +1,21 @@
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlunosComponent } from './alunos.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunosRoutingModule } from './alunos.routing.module';
+
+@NgModule({
+  declarations: [
+    AlunosComponent, 
+    AlunoFormComponent, 
+    AlunoDetalheComponent
+  ],
+  
+  imports: [
+    CommonModule,
+    AlunosRoutingModule
+  ]
+})
+export class AlunosModule { }
---
+++ src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AlunosComponent } from './alunos.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+
+const alunosRoutes = [
+    //  Declarando rotas filhas, interessante para permitir 
+    //que mais de um componente seja exibido
+    { path: 'alunos', component: AlunosComponent, children : [ //Pai
+        //  Para evitar colisão das rotas alunos/novo vem 
+        //antes das rotas com variaveis, como no caso do :id
+        { path: 'novo', component: AlunoFormComponent },       //Filho
+        { path: ':id', component: AlunoDetalheComponent },     //Filho
+        { path: ':id/editar', component: AlunoFormComponent }  //Filho  
+    ]}
+]
+
+@NgModule({
+    imports: [RouterModule.forChild(alunosRoutes)],
+    exports: [RouterModule]
+})
+export class AlunosRoutingModule {
+
+}
